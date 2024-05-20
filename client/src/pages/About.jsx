e9ce3f318e64f873ed6f8fea4e60125640c82413
Kd098 <112809345+Kd098@users.noreply.github.com>
2024-05-20 18:04:08 +0530

    Initial commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About RENTIFY</h1>
+      <p className='mb-4 text-slate-700'>
+
+      Welcome to Rentify, where your perfect getaway begins. We are dedicated to offering exceptional vacation rental properties that cater to all your needs and desires, ensuring that your stay is not just comfortable but truly memorable.</p>
+      <p className='mb-4 text-slate-700'>
+      At Rentify, our mission is to provide a seamless and delightful rental experience for our guests. We strive to offer a diverse selection of high-quality homes, from cozy cottages and beachfront villas to urban lofts and mountain retreats. Each property is carefully curated to meet our high standards of comfort, style, and convenience.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}
